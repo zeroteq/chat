bd56d63a07d3de9a796a64cd762febe42e0fc1e7
zeroteq <99808976+zeroteq@users.noreply.github.com>
2022-02-16 16:38:38 +0200

    Add files via upload

---
+++ data/config/config.js
@@ -0,0 +1,25 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgEsvVdfs10Ac8iEij1OmCSM8AJt7bKyA",
+
+
+  authDomain: "global-connect-4c0ae.firebaseapp.com",
+
+
+  databaseURL: "https://global-connect-4c0ae-default-rtdb.firebaseio.com",
+
+
+  projectId: "global-connect-4c0ae",
+
+
+  storageBucket: "global-connect-4c0ae.appspot.com",
+
+
+  messagingSenderId: "712174232975",
+
+
+  appId: "1:712174232975:web:8d4fdbd0e45fb979de57fa",
+
+
+  measurementId: "G-YJRE9BPJ0B"
+};
+const app = firebase.initializeApp(firebaseConfig);
